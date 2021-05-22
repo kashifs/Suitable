903301ccad6c7de58fd98bbc6bd44db7645b85c9
Kashif Smith <smithkashif@gmail.com>
2021-05-22 16:07:13 -0400

    refactored logic to Firstragment, shows what to wear based off temperature

--- app/src/main/java/com/example/suitable/FirstFragment.java
+++ app/src/main/java/com/example/suitable/FirstFragment.java
@@ -1,15 +1,77 @@
 package com.example.suitable;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class FirstFragment extends Fragment {
+
+    //https://home.openweathermap.org/api_keys TODO best practice for this
+    public final static String OWM_APIKEY = "fe7aafe463cc341de6052036b3e47fcf";
+
+    //    public final static String NEW_YORK = "New York, US";
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String[] shortTops = {
+            "Blue & White Striped Shirt", "V Neck White Tee", "Off White Tee"
+    };
+
+    public static final String[] longTops = {
+            "Blue OCBD #1", "Blue OCBD #2", "Blue OCBD #3",
+            "White OCBD #1", "WhiteOCBD #2", "WhiteOCBD #3",
+            "Jean Shirt", "Black Long T Shirt", "Black Jean Shirt",
+    };
+
+    public static final String[] formalTops = {
+            "Beige Dress Shirt"
+    };
+
+    public static final String[] shortBottoms = {
+            "Blue Chino Shorts", "Green Chino Shorts",
+            "Khaki Chino Shorts", "Navy Chino Shorts"
+    };
+
+    public static final String[] longBottoms = {
+            "Dark Jeans", "Khaki Chinos",
+            "Olive Chinos", "Black Chinos", "Navy Chinos"
+    };
+
+    public static final String[] zapatos = {
+            "White Sneakers", "Grey Sneakers", "Brown Loafers",
+            "Blue and White Sneakers", "Brown Boots"
+    };
+
+    Random topsRand = new Random();
+    Random bottomsRand = new Random();
+    Random zapatosRand = new Random();
+
+    String finalTop;
+    String finalBottom;
+    String finalZapato;
+
+
+    RequestQueue requestQueue;
+    StringRequest stringRequest;
 
     @Override
     public View onCreateView(
@@ -23,12 +85,114 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Instantiate the RequestQueue.
+        requestQueue = Volley.newRequestQueue(getContext());
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=5128581&units=imperial&appid=" + OWM_APIKEY;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            JSONObject main = response.getJSONObject("main");
+                            Log.e(TAG, "Main: " + main.toString());
+                            double feelsLike = Double.parseDouble(main.getString("feels_like"));
+                            double humidity = Double.parseDouble(main.getString("humidity"));
+
+                            Log.e(TAG, "Feels Like: " + feelsLike);
+                            Log.e(TAG, "Humidity: " + humidity);
+
+//                            feelsLike = 95;
+
+
+                            if (feelsLike >= 90) {
+
+                                finalTop = shortTops[topsRand.nextInt(shortTops.length)];
+                                finalBottom = shortBottoms[bottomsRand.nextInt(shortBottoms.length)];
+                                finalZapato = zapatos[zapatosRand.nextInt(zapatos.length - 1)];
+
+                                Log.e(TAG, "Today's Top: " + finalTop);
+                                Log.e(TAG, "Today's Bottom: " + finalBottom);
+                                Log.e(TAG, "Today's Zapato: " + finalZapato);
+
+                            } else if (feelsLike <= 85 && feelsLike > 75) {
+
+                                String[] allTops = Arrays.copyOf(shortTops, shortTops.length + longTops.length);
+                                System.arraycopy(longTops, 0, allTops, shortTops.length, longTops.length);
+
+                                String[] allBottoms = Arrays.copyOf(shortBottoms, shortBottoms.length + longBottoms.length);
+                                System.arraycopy(longBottoms, 0, allBottoms, shortBottoms.length, longBottoms.length);
+
+                                finalTop = allTops[topsRand.nextInt(allTops.length)];
+                                finalBottom = allBottoms[bottomsRand.nextInt(allBottoms.length)];
+                                finalZapato = zapatos[zapatosRand.nextInt(zapatos.length)];
+
+                                Log.e(TAG, "Today's Top: " + finalTop);
+                                Log.e(TAG, "Today's Bottom: " + finalBottom);
+                                Log.e(TAG, "Today's Zapato: " + finalZapato);
+                            } else {
+
+                                String[] allTops = Arrays.copyOf(shortTops, shortTops.length + longTops.length);
+                                System.arraycopy(longTops, 0, allTops, shortTops.length, longTops.length);
+
+                                finalTop = allTops[topsRand.nextInt(allTops.length)];
+                                finalBottom = longBottoms[bottomsRand.nextInt(longBottoms.length)];
+                                finalZapato = zapatos[zapatosRand.nextInt(zapatos.length)];
+
+                                Log.e(TAG, "Today's Top: " + finalTop);
+                                Log.e(TAG, "Today's Bottom: " + finalBottom);
+                                Log.e(TAG, "Today's Zapato: " + finalZapato);
+                            }
+
+                            TextView textview_top = view.findViewById(R.id.textview_top);
+                            TextView textview_bottom = view.findViewById(R.id.textview_bottom);
+                            TextView textview_zapato = view.findViewById(R.id.textview_zapato);
+
+                            textview_top.setText("Today it feels like " + feelsLike + "\n" + "Today's Top: " + finalTop);
+                            textview_bottom.setText("Today's bottom: " + finalBottom);
+                            textview_zapato.setText("Today's footwear: " + finalZapato);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("MainActivity.java", "That didn't work");
+                    }
+                });
+
+        // Set the tag on the request.
+        if (stringRequest != null) {
+            stringRequest.setTag(TAG);
+        }
+
+        // Add the request to the RequestQueue.
+        if (requestQueue != null) {
+            requestQueue.add(jsonObjectRequest);
+        }
+
+
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (requestQueue != null) {
+            requestQueue.cancelAll(TAG);
+        }
+    }
+
 }
--- app/src/main/java/com/example/suitable/MainActivity.java
+++ app/src/main/java/com/example/suitable/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.suitable;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import com.android.volley.Request;
@@ -17,12 +16,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,49 +30,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    //https://home.openweathermap.org/api_keys TODO best practice for this
-    public final static String OWM_APIKEY = "fe7aafe463cc341de6052036b3e47fcf";
 
-    //    public final static String NEW_YORK = "New York, US";
-    public static final String TAG = MainActivity.class.getSimpleName();
-    public static final String[] shortTops = {
-            "BlueWhiteStriped", "VNeckWhite", "OffWhite"
-    };
-
-    public static final String[] longTops = {
-            "BlueOCBD_1", "BlueOCBD_2", "BlueOCBD_3",
-            "WhiteOCBD_1", "WhiteOCBD_2", "WhiteOCBD_3",
-            "JeanShirt", "BlackTShirt", "BlackJeanShirt",
-    };
-
-    public static final String[] formalTops = {
-            "BeigeShirt"
-    };
-
-    public static final String[] shortBottoms = {
-            "BlueChinoShort", "GreenChinoShort", "KhakiChinoShort", "NavyChinoShort"
-    };
-
-    public static final String[] longBottoms = {
-            "DarkJean", "KhakiChino", "OliveChino", "BlackChino", "NavyChino"
-    };
-
-    public static final String[] zapatos = {
-            "WhiteS", "GreyS", "BrownL", "BlueWhiteS", "BrownB"
-    };
-
-    Random topsRand = new Random();
-    Random bottomsRand = new Random();
-    Random zapatosRand = new Random();
-
-    String finalTop;
-    String finalBottom;
-    String finalZapato;
-
-//    int n = topsRand.nextInt(20);
-
-    RequestQueue requestQueue;
-    StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,84 +40,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        // Instantiate the RequestQueue.
-        requestQueue = Volley.newRequestQueue(this);
-        String url = "http://api.openweathermap.org/data/2.5/weather?id=5128581&units=imperial&appid=" + OWM_APIKEY;
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-//                        Log.e(TAG, "JSONObject response is: "+ response.toString());
-
-                        try {
-
-                            JSONObject main = response.getJSONObject("main");
-                            Log.e(TAG, "Main: " + main.toString());
-                            double feelsLike = Double.parseDouble(main.getString("feels_like"));
-                            double humidity = Double.parseDouble(main.getString("humidity"));
-
-                            Log.e(TAG, "Feels Like: " + feelsLike);
-                            Log.e(TAG, "Humidity: " + humidity);
-
-                            feelsLike = 70;
-
-                            int numTops;
-                            int numBottoms;
-
-                            if (feelsLike >= 90) {
-                                Log.e(TAG, "Today's Top: " + shortTops[topsRand.nextInt(shortTops.length)]);
-                                Log.e(TAG, "Today's Bottom: " + shortBottoms[bottomsRand.nextInt(shortBottoms.length)]);
-                                Log.e(TAG, "Today's Zapato: " + zapatos[zapatosRand.nextInt(zapatos.length - 1)]);
-
-                            }
-                            else if (feelsLike <= 85 && feelsLike > 75) {
-
-                                String[] allTops = Arrays.copyOf(shortTops, shortTops.length + longTops.length);
-                                System.arraycopy(longTops, 0, allTops, shortTops.length, longTops.length);
-
-                                String[] allBottoms = Arrays.copyOf(shortBottoms, shortBottoms.length + longBottoms.length);
-                                System.arraycopy(longBottoms, 0, allBottoms, shortBottoms.length, longBottoms.length);
-
-                                Log.e(TAG, "Today's Top: " + allTops[topsRand.nextInt(allTops.length)]);
-                                Log.e(TAG, "Today's Bottom: " + allBottoms[bottomsRand.nextInt(allBottoms.length)]);
-                                Log.e(TAG, "Today's Zapato: " + zapatos[zapatosRand.nextInt(zapatos.length)]);
-                            }
-                            else{
-
-                                String[] allTops = Arrays.copyOf(shortTops, shortTops.length + longTops.length);
-                                System.arraycopy(longTops, 0, allTops, shortTops.length, longTops.length);
-
-                                Log.e(TAG, "Today's Top: " + allTops[topsRand.nextInt(allTops.length)]);
-                                Log.e(TAG, "Today's Bottom: " + longBottoms[bottomsRand.nextInt(longBottoms.length)]);
-                                Log.e(TAG, "Today's Zapato: " + zapatos[zapatosRand.nextInt(zapatos.length)]);
-                            }
-
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("MainActivity.java", "That didn't work");
-                    }
-                });
-
-        // Set the tag on the request.
-        if (stringRequest != null) {
-            stringRequest.setTag(TAG);
-        }
-
-        // Add the request to the RequestQueue.
-        if (requestQueue != null) {
-            requestQueue.add(jsonObjectRequest);
-        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -196,13 +73,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (requestQueue != null) {
-            requestQueue.cancelAll(TAG);
-        }
-    }
+
 
 
 }
